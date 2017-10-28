9a5d6c9d8d730f6f1de66c027ae255229496ee99
mezeitune <matizeitune@gmail.com>
2017-10-28 20:29:18 -0300

    Robar Talisman , y implementacion de Option para la facilidad (ver como desacoplar el return del Option en caso de modificacion del tipo)

--- src/model/Tarea.scala
+++ src/model/Tarea.scala
@@ -1,11 +1,14 @@
 package model
 
+//NOSE COMO HACER PARA QUE CUANDO HEREDE DEL OPTION , NO ESTE ATADO A LA MONADA DE TENER QUE DEVOLVER POR EJEMPLLO SOME(10), YA QUE
+//SI HABRIA QUE CAMBIAR ESO POR UNA LIST TE CAGA EL MODELO
 
+//TODAS LAS TAREAS TIENEN LOS MISMOS PARAMETROS , COMO HAGO PARA EVITAR ESCRIBIRMELOS SIEMPRE QUE HEREDO DE TAREA?????
 
 
 trait Tarea{
 
-  def facilidad()
+  def facilidad() : Option[Int]
   
 
   
@@ -15,11 +18,11 @@ trait Tarea{
 
 class PelearContraMonstruo (modificaciones: List[Modificacion], heroe: Heroe, equipo: Equipo) extends Tarea{
   
-    def facilidad(){
+    def facilidad():Option[Int] ={
        
         equipo.lider() match {
-          case g:Guerrero            => 20
-          case _                     => 10
+          case g:Guerrero            => Some(20)
+          case _                     => Some(10)
           
           
         }
@@ -31,14 +34,30 @@ class PelearContraMonstruo (modificaciones: List[Modificacion], heroe: Heroe, eq
 
 class ForzarPuerta (modificaciones: List[Modificacion], heroe: Heroe, equipo: Equipo) extends Tarea{
   
-    def facilidad(){
+    def facilidad():Option[Int] ={
        
-     heroe.getStat("inteligencia") + equipo.heroes.foldLeft(0) {(acum, heroe) => 
+     Some(heroe.getStat("inteligencia") + equipo.heroes.foldLeft(0) {(acum, heroe) => 
       heroe match {
         case l:Ladron                => acum + 10
         case _                       => 0
         }
-      }
+      })
+        
+    }
+  
+  
+}
+
+class RobarTalisman (modificaciones: List[Modificacion], heroe: Heroe, equipo: Equipo) extends Tarea{
+  
+    def facilidad():Option[Int] ={
+       
+        equipo.lider() match {
+          case l:Ladron            => Some(heroe.getStat("velocidad"))
+          case _                   => None
+          
+          
+        }
         
     }
   
