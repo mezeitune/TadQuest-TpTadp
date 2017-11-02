453d89c724fc682d1b9fddd7b2ab2bcac6e81bd4
mbaridonn <mbaridonn@gmail.com>
2017-11-01 21:23:44 -0300

    Tests: equipar item a heroe

--- test/model/HeroesTests.scala
+++ test/model/HeroesTests.scala
@@ -9,12 +9,27 @@ import org.junit.Assert
 
 class HeroesTests {
   
-  @Test
-  def algo(): Unit = {
-
-
-    Assert.assertEquals(None, Some(None).flatten)
+  var heroe: Heroe = null
+  var espada: Item = null
+  var lanza: Item = null
+  
+  @Before
+  def setup(){
+    heroe = new Heroe()
+    espada = new Item(List(Cabeza,Torso,Mano),List((heroe => heroe.stats.get(Fuerza).get > 50)),List(),200)
+    lanza = new Item(List(Cabeza,Torso,Mano),List((heroe => heroe.stats.get(Fuerza).get > 200)),List(),200)
   }
   
+  @Test
+  def heroeEquipaItemCorrectamente() = {
+    val heroeEquipado = heroe.equipar(espada)
+    Assert.assertTrue(heroeEquipado.inventario.contains(espada))
+  }
+   
+  @Test
+  def heroeNoPuedeEquiparItemPorRestriccion(){
+    val heroeEquipado = heroe.equipar(lanza)
+    Assert.assertFalse(heroeEquipado.inventario.contains(lanza))
+  }
   
 }
