b33e747629ae7db84f72bdc74a77ad88f24e8e84
martingsalazar <martin.salazar20@gmail.com>
2017-11-03 02:00:23 -0300

    Tests de heroe para desequipar, aplicaciones de modificaciones, etc.

--- test/model/HeroesTests.scala
+++ test/model/HeroesTests.scala
@@ -7,12 +7,14 @@ import org.junit.Assert
 class HeroesTests {
   
   var heroe: Heroe = null
+  var mago: Heroe = null
   var espada: Item = null
   var lanza: Item = null
   
   @Before
   def setup(){
     heroe = new Heroe().trabajo(Guerrero)
+    mago = new Heroe().trabajo(Mago)
     espada = new Item(List(Mano), List((heroe => heroe.stats.get(Fuerza).get > 50)), List(VariarStatEn(Fuerza,100)), 200)
     lanza = new Item(List(Mano), List((heroe => heroe.stats.get(Fuerza).get > 200)), List(), 250)
   }
@@ -41,6 +43,32 @@ class HeroesTests {
   def heroeCambiaDeTrabajo(){
     val heroeNuevoTrabajo = heroe.trabajo(Mago)
     Assert.assertEquals(Mago, heroeNuevoTrabajo.trabajo)
-  } 
+  }
+  
+  @Test
+  def elStatPrincipalDelMagoEs120DeInteligencia(){
+    Assert.assertEquals(120, mago.valorStatPrincipal())
+  }
+  
+  @Test
+  def HeroeDesequipaItemCorrectamente(){
+    val heroeEquipado = heroe.equipar(espada)
+    val heroeDesequipado = heroeEquipado.desequipar(espada)
+    Assert.assertFalse(heroeDesequipado.inventario.contains(espada))
+  }
+  
+  @Test
+  def elStatBaseDelHeroeAumentaCorrectamente(){
+    val fuerzaOriginal = heroe.getStat(Fuerza)
+    val heroeConMayorStat = heroe.incrementarStatBase(Fuerza, 100)
+    Assert.assertEquals(fuerzaOriginal+100,heroeConMayorStat.getStat(Fuerza))
+  }
+  
+  @Test
+  def seVariaLaFuerzaEn100YSeIncrementanStatsEnPorcentajeDePrincipalPor50Correctamente(){
+    val modificaciones: List[Modificacion] =  List(VariarStatEn(Fuerza,100),IncrementarStatsEnPorcentajeDePrincipal(50))
+    val heroeModificado = heroe.aplicarModificaciones(modificaciones)
+    Assert.assertEquals(5965,heroeModificado.getStat(Fuerza))
+  }
   
 }
