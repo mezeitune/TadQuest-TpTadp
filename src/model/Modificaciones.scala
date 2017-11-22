ff98d49c95cf6ce5816be26a43001db08c3973bc
Nico <nmrainhart@gmail.com>
2017-11-22 15:07:33 -0300

    Eliminada prioridad de las Modificaciones.
    Ahora se ordenan a partir de la lista del OrdenadorModificaciones.

--- src/model/Modificaciones.scala
+++ src/model/Modificaciones.scala
@@ -6,25 +6,21 @@ object Modificacion{
       val ordenModificaciones = List(AgregarItem,VariarStatEn,VariarStatEnSi,ModificarStatPorCantidadItems,
           IncrementarStatsEnPorcentajeDePrincipal,SetearStat,SetearStatSegun,ModificarTodosLosStats)
       ordenModificaciones.indexOf(a.getClass) - ordenModificaciones.indexOf(b.getClass)
-      //a.prioridad.compare(b.prioridad) //DEFINIR BIEN FORMA DE COMPARAR (!!!)
     }
   }
 }
 
 sealed trait Modificacion{
-  val prioridad: PrioridadModificacion
   def apply(heroe: Heroe): Heroe
 }
 
 case class AgregarItem(itemAAgregar: Item) extends Modificacion {
-  val prioridad = ALTA
   def apply(heroe: Heroe) = heroe.equipar(itemAAgregar).getOrElse(heroe)//DEBERIA FALLAR SI NO SE PUEDE EQUIPAR, PERO ESO ROMPERIA LA FIRMA
 }
 
 sealed trait ModificacionStat extends Modificacion
 
 case class ModificarStatPorCantidadItems(stat: Stat, valor: Int) extends ModificacionStat {
-  val prioridad = MEDIA
   def apply(heroe: Heroe) = {
     val cantItems = heroe.inventario.size
     val valorAumentado = cantItems * valor
@@ -33,29 +29,26 @@ case class ModificarStatPorCantidadItems(stat: Stat, valor: Int) extends Modific
 }
 
 case class SetearStatSegun(statAModificar: Stat , stat: Stat) extends ModificacionStat {
-  val prioridad = BAJA
   def apply(heroe: Heroe) = {
     heroe.setStatBase(stat, heroe.stats.get(stat).get)
   }
 }
 
 case class ModificarTodosLosStats(valor: Int) extends ModificacionStat {
-  val prioridad = BAJA
   def apply(heroe: Heroe) = {
     heroe.stats.keys.foldLeft(heroe) {(heroe,stat) => heroe.setStatBase(stat, 1)}
   }
 }
 
 case class VariarStatEn(stat: Stat, valor: Int) extends ModificacionStat {
-  val prioridad = ALTA
   def apply(heroe: Heroe) = heroe.incrementarStatBase(stat, valor)
 }
+
 case class SetearStat(stat: Stat, valor: Int) extends ModificacionStat {
-  val prioridad = BAJA
   def apply(heroe: Heroe) = heroe.setStatBase(stat, valor)
 }
+
 case class IncrementarStatsEnPorcentajeDePrincipal(porcentaje: Float) extends ModificacionStat {
-  val prioridad = MEDIA
   def apply(heroe: Heroe) = {
     val incremento = (heroe.valorStatPrincipal * porcentaje).round
     heroe.stats.keys.foldLeft(heroe) {(heroe, stat) => heroe.incrementarStatBase(stat, incremento)}
@@ -63,7 +56,6 @@ case class IncrementarStatsEnPorcentajeDePrincipal(porcentaje: Float) extends Mo
 }
 
 case class VariarStatEnSi(stat: Stat, valor: Int, condicion: Restriccion) extends ModificacionStat {
-  val prioridad = ALTA
   def apply(heroe: Heroe)={
     var aMod=0
     if(condicion(heroe)){
@@ -72,11 +64,3 @@ case class VariarStatEnSi(stat: Stat, valor: Int, condicion: Restriccion) extend
     heroe.incrementarStatBase(stat, aMod)
   }
 }
-
-
-sealed abstract class PrioridadModificacion (val prioridad: Int) extends Ordered[PrioridadModificacion]{
-  def compare(that: PrioridadModificacion) = this.prioridad - that.prioridad
-}
-case object ALTA extends PrioridadModificacion(3)
-case object MEDIA extends PrioridadModificacion(2)
-case object BAJA extends PrioridadModificacion(1)
